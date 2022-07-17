3e5ed552a8521da676d5c954bcbbda2982dfa370
Arm Bun <bellotaophyc@outlook.com>
2022-07-17 11:41:06 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:667f9092c2edb6eaeea162a3ff871f0eecea605c5da00593168dade18eee474e
+size 530
---
+++ vairables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0d7d7b55a4e8700d504b027d516d251782e500a6e2caa1a31c37b683d77ea11
+size 152
